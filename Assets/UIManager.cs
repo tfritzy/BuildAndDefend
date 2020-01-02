40ff4408879a5d795db32f6fbc23c82569d13cce
Tyler Fritz <tfritzy@gmail.com>
2020-01-01 20:21:52 -0800

    Added Upgrade menu

---
+++ Assets/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private GameObject canvas;
+    void Start(){
+        this.canvas = GameObject.Find("Canvas");
+    }
+
+    private const string upgradeMenuPath = "Gameobjects/UI/KineticUpgradeMenu";
+    public void OpenUpgradeMenu(){
+        GameObject upgradeWindow = Resources.Load<GameObject>(upgradeMenuPath);
+        Instantiate(upgradeWindow, Vector3.zero, new Quaternion(), this.canvas.transform);
+    }
+}

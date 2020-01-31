51cfe32c69c7cf3775ae4c8d369a978d054129f8
Tyler Fritz <tfritzy@gmail.com>
2020-01-31 10:23:45 -0800

    Made harvester collect variable amounts of resources based on the surrounding environment

--- Assets/Scripts/Buildings/Buildings/ResourceCollection/Harvester.cs
+++ Assets/Scripts/Buildings/Buildings/ResourceCollection/Harvester.cs
@@ -1,10 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 public abstract class Harvester : Building
 {
-    public abstract ResourceDAO ResourceProductionPerHour { get; }
+    public ResourceDAO ResourceProductionPerHour { get; set; }
+    protected abstract int resourceGatherRange { get; }
+    protected Dictionary<EnvironmentTileType, int> gatherRateByTileType { get; set; }
 
     public override void Setup()
     {
         base.Setup();
+        this.gatherRateByTileType = GetResourceProductionByTileType();
+        this.ResourceProductionPerHour = CalculateResourceProduction(this.Position);
         RegisterResourceProduction();
     }
 
@@ -34,5 +41,41 @@ public abstract class Harvester : Building
         }
 
         Map.Harvesters.Remove(BuildingId);
+    }
+
+    protected EnvironmentTileType[,] GetSurroundingEnvironment(Vector2Int size, Vector2Int offset)
+    {
+        EnvironmentTileType[,] tiles = new EnvironmentTileType[size.x + 1, size.y + 1];
+
+        int x = Mathf.Max(this.Position.x - size.x / 2 + offset.x, 0);
+        int xMax = Mathf.Min(this.Position.x + size.x / 2 + offset.x, Map.Environment.GetLength(0) - 1);
+        int yMax = Mathf.Min(this.Position.y + size.y / 2 + offset.y, Map.Environment.GetLength(1) - 1);
+        for (; x <= xMax; x++)
+        {
+            for (int y = Mathf.Max(this.Position.y - size.y / 2 + offset.y, 0); y <= yMax; y++)
+            {
+                Debug.Log($"{x - this.Position.x + size.x / 2},{y - this.Position.y + size.y / 2}");
+                tiles[x - this.Position.x + size.x / 2, y - this.Position.y + size.y / 2] = Map.Environment[x, y].Type;
+            }
+        }
+
+        return tiles;
+    }
+
+    public abstract Dictionary<EnvironmentTileType, int> GetResourceProductionByTileType();
+
+    protected int brushWoodGatherRate = 3;
+    protected virtual ResourceDAO CalculateResourceProduction(Vector2Int position)
+    {
+        ResourceDAO resourceProductionPerHour = new ResourceDAO();
+        foreach (EnvironmentTileType type in GetSurroundingEnvironment(new Vector2Int(this.Size.x + 2, this.Size.y + 2), Vector2Int.zero))
+        {
+            if (type == EnvironmentTileType.Brush)
+            {
+                resourceProductionPerHour.Add(new ResourceDAO(wood: brushWoodGatherRate));
+            }
+        }
+
+        return resourceProductionPerHour;
     }
 }
--- Assets/Scripts/Buildings/Buildings/ResourceCollection/WoodHarvesters/LumberCamp.cs
+++ Assets/Scripts/Buildings/Buildings/ResourceCollection/WoodHarvesters/LumberCamp.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LumberCamp : Harvester
@@ -9,5 +10,16 @@ public class LumberCamp : Harvester
     public override PathableType PathableType => PathableType.UnPathable;
     public override Faction Faction => Faction.All;
     public override ResourceDAO PowerUpCost => new ResourceDAO(wood: 2 * this.Tier, gold: 3 * this.Tier);
-    public override ResourceDAO ResourceProductionPerHour => new ResourceDAO(wood: 10);
+    protected override int resourceGatherRange => 1;
+
+    public override Dictionary<EnvironmentTileType, int> GetResourceProductionByTileType()
+    {
+        return new Dictionary<EnvironmentTileType, int>
+        {
+            {
+                EnvironmentTileType.Brush,
+                3
+            },
+        };
+    }
 }
